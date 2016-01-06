2ba888e0cc5c6e671d7c029e5d48d44adebaf7d3
Nikolay Murga <nikolay.m@murga.kiev.ua>
2016-01-06 22:49:08 +0200

    add logged_log

--- lib/iptables_web/cli/import.rb
+++ lib/iptables_web/cli/import.rb
@@ -4,5 +4,5 @@ require 'forwardable'
 $terminal = IptablesWeb::Cli::LoggedOutput.new
 module Kernel
   extend Forwardable
-  def_delegators :$terminal, :agree, :ask, :choose, :say, :logged_say
+  def_delegators :$terminal, :agree, :ask, :choose, :say, :logged_say, :logged_log
 end
